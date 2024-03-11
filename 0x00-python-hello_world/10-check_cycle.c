1dec94dfa3da242cd05083aff83450a58413a39f
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-12 00:29:09 +0100

    Task 10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *temp = list;
 
-	while (temp->next != NULL && temp->next->next != NULL)
+	while (temp != NULL && temp->next != NULL)
 	{
 		temp = temp->next;
 
