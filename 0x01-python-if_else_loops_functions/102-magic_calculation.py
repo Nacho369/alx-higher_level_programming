d8167c6ff38bf1c4e85a48cf494daccb6f70a425
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-13 20:50:28 +0100

    Task 16. ByteCode -> Python #2

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -3,9 +3,8 @@
 def magic_calculation(a, b, c):
     if not (a < b):
         return (c)
+    elif (c > b):
+        return (a + b)
     else:
-        if (c > b):
-            return (a + b)
-        else:
-            return ((a * b) - c)
+        return ((a * b) - c)
             
