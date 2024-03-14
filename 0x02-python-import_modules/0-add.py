25577fd657015b113451e858fa26fdf4341679fb
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-14 22:27:46 +0100

    Task 0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,9 +5,8 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    res = add(a, b)
 
-    print ("{} + {} = {}".format(a, b, res))
+    print ("{} + {} = {}".format(a, b, add(a, b)))
 
 if __name__ == '__main__':
     main()
