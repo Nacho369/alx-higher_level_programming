ee0577cc9ed6c573804764c292e5882a1f070bb9
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-15 00:04:07 +0100

    Task 3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+import sys
+
+
+def main():
+    argv = sys.argv
+    summ = 0
+
+    for ind, num in enumerate(argv):
+        if (ind == 0):
+            continue
+
+        summ += int(num)
+
+    print("{}".format(summ))
+
+
+if __name__ == "__main__":
+    main()
