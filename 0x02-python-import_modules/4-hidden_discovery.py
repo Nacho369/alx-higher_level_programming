1665d1d67c776821d2d573a69f43c0768f36b2ac
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-15 04:29:14 +0100

    Task 5. Everything can be imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,9 +3,10 @@ import hidden_4
 
 
 def main():
-    for x in dir(hidden_4):
-        if "__" not in x:
-            print(X)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
 
 
 if __name__ == "__main__":
