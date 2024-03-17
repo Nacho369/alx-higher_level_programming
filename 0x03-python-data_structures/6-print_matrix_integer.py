46b164430bf6d992302a7ec9c55604c7c69620c4
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-17 14:19:47 +0100

    Task 0 Edit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    # [print(elem) for row in matrix for elem in row]
+
+    for row in matrix:
+        for col in row:
+            print("{}".format(col), end=" ")
+            if row
