175fbf47acc471b02f001eee0e574291ba812029
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-17 17:55:52 +0100

    Task 3 Edit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,6 @@ def print_reversed_list_integer(my_list=[]):
     rev = my_list.copy()
     rev.reverse()
 
-    for val in rev:
-        print("{:d}".format(val))
+    if rev:
+        for val in rev:
+            print("{:d}".format(val))
