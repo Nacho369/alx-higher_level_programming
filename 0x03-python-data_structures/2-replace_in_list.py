29f066afa54928fb1f9b48420ba06d41b0f5880f
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-17 14:22:08 +0100

    Task 2 Edit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,7 +6,7 @@ def replace_in_list(my_list, idx, element):
         return (my_list)
 
     if idx > (len(my_list) - 1):
-        rerurn(my_list)
+        return (my_list)
 
     my_list[idx] = element
 
