51e8e0018bcacab441e766ca4bdc245c905e2736
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-17 21:32:09 +0100

    Task 7. Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,5 +14,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_b) < 2):
         tuple_b = tuple_b + (0,)
 
-    new_tup = ((tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]))
+    new_tup = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
     return (new_tup)
