7a3142b00d5bbe856c74f2e70c736aac1231d11a
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-17 18:00:58 +0100

    Task 6. Lists of lists = Matrixv

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,7 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    # [print(elem) for row in matrix for elem in row]
-
     for row in matrix:
         for col in row:
-            print("{}".format(col), end=" ")
+            print("{:d}".format(col), end=" ")
         print("")
