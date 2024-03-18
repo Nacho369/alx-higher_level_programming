4295c7d8401bae09103174b645f8faec0df26d5f
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-18 14:50:12 +0100

    Task 10. Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,5 +2,9 @@
 
 
 def divisible_by_2(my_list=[]):
-    new_list = [True if my_list[ind] % 2 == 0 else False for ind, val in enumerate(my_list)]
+    new_list = [True
+                if my_list[ind] % 2 == 0
+                else False
+                for ind, val in enumerate(my_list)]
+
     return (new_list)
