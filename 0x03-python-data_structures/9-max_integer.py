db4464b040a0c53fdae9f5c1a5c5e7e8f058dfce
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-18 14:34:14 +0100

    Task 9. Find the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if (len(my_list) == 0):
         return (None)
 
-    max_int = 0
+    max_int = my_list[0]
     for val in my_list:
         if (val > max_int):
             max_int = val
