1766b214bc8ebea64696cb3b5009d2ac29d5023c
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-20 22:11:02 +0100

    modified:   0x03-python-data_structures/13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,5 +3,12 @@
 
 int is_palindrome(listint_t **head)
 {
+	listint_t *current = *head;
+	listint_t *forward = *head;
 
+	while (cureent != NULL)
+	{
+		current = current->next;
+		forward = forward->next->next;
+	}
 }
