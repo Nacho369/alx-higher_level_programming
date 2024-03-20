dea501e44019388480cb898809979fa044132371
Fortune Iheanacho <nachodev369@gmail.com>
2024-03-20 22:32:01 +0100

    Task 3. Present in both

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    new_set = set()
+
+    for x in set_1:
+        for y in set_2:
+            if x == y:
+                new_set.add(x)
+
+    return (new_set)
