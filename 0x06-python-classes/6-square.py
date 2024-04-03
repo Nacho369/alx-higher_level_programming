2d9edf365fc616f84c8b563b84b65659b449a645
Fortune Iheanacho <nachodev369@gmail.com>
2024-04-03 22:44:48 +0100

    Task 6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,15 +40,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """Set the current size of the Square Object
-
-        Args:
-            value: Value of the position
-        """
         if (not isinstance(value, tuple) or
-            len(value) != 2 or
-            not all(val >= 0 for val in value) or
-                not all(isinstance(val, int) for val in value)):
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
