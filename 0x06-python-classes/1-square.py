04efa1771adca85c04ac19bd65fc48b4fdc88d60
Fortune Iheanacho <nachodev369@gmail.com>
2024-04-03 12:24:16 +0100

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a Square Class"""
+
+
+class Square:
+    """Represents a Square Object"""
+
+    def __init__(self, size):
+        """Initialize the class instance
+
+        Args:
+            size: Size of the Square object
+        """
+        self.__size = size
