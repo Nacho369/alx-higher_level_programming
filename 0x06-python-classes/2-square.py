1f229ae5eddd29748b145224f6bf009dfd3610be
Fortune Iheanacho <nachodev369@gmail.com>
2024-04-03 13:04:51 +0100

    Task 2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a Square Class"""
+
+
+class Square:
+    """Represents a Square Object"""
+
+    def __init__(self, size=0):
+        """Initialize a Square instance
+
+        Args:
+            size: Size of the Square Instance
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
