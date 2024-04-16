277981b76a42b9ef67fb44ed6f25226d5f824cba
Fortune Iheanacho <nachodev369@gmail.com>
2024-04-16 03:59:06 +0100

    Task 2 and 4 Edit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -14,7 +14,7 @@ def is_same_class(obj, a_class):
 
     Return: True if obj is an instance of a_class otherwise False
     """
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
 
     return False
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,7 +12,7 @@ def inherits_from(obj, a_class):
         If obj is an inherited instance of a_class - True.
         Otherwise - False.
     """
-    if isinstance(obj, a_class) and not isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return (True)
 
     return (False)
