cbe17a901448d8b00005a22ba0557c91cadb53b6
Fortune Iheanacho <nachodev369@gmail.com>
2024-04-18 00:55:19 +0100

    Task 9. Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" Defines a class Student that defines a student
+"""
+
+
+class Student:
+    """
+    A module class student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        A __init__ method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        A to_json method
+        """
+        return (self.__dict__)
