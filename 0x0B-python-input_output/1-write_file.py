6983757493e0169f4bd64106b6fcc55a7dd62896
Fortune Iheanacho <nachodev369@gmail.com>
2024-04-17 23:34:28 +0100

    Task 2. Append to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-function that writes a string to a text file (UTF8)
+Function that writes a string to a text file (UTF8)
 and returns the number of characters written
 """
 
 
 def write_file(filename="", text=""):
-    """ module write_file
+    """ Module write_file
     """
-    with open(filename, 'w') as f:
-        return f.write(text)
+    with open(filename, 'w', encoding="utf-8") as file_a:
+        return file_a.write(text)
