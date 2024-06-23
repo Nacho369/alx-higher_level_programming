fdc26ee7df4a40ad81fb852a418847c98d365060
Fortune Iheanacho <nachodev369@gmail.com>
2024-06-23 09:18:33 +0100

    Task 3. SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A script that takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa where name
+matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=argv[1],
+                           passwd=argv[2], db=argv[3], port=3306)
+
+    cursor = conn.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+    cursor.execute(query, (argv[4],))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        if row[1] == argv[4]:
+            print(row)
+
+    cursor.close()
+    conn.close()
