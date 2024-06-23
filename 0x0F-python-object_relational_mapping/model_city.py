c927d0a8bca6950fc42eafefc5b124cccfb6146a
Fortune Iheanacho <nachodev369@gmail.com>
2024-06-23 19:09:36 +0100

    Task 14. Cities in state

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A python file that contains the class definition of a
+City and an instance Base = declarative_base()
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    '''
+        Creates table for cities
+    '''
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
+    name = Column(String(128), nullable=False)
