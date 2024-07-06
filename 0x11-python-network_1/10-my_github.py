e64ab1620dfddda37865439ca9cd5a4221458446
Fortune Iheanacho <nachodev369@gmail.com>
2024-07-06 04:53:40 +0100

    Task 9. My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    pat = sys.argv[2]
+    auth = HTTPBasicAuth(username, pat)
+    url = 'https://api.github.com/user'
+    f = requests.get(url,
+                     auth=auth)
+    print(f.json().get('id'))
