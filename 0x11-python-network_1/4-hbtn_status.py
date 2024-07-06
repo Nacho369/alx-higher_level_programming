b6a3c0cd0f38a9eff6e6c567187e3e0eaa42aff9
Fortune Iheanacho <nachodev369@gmail.com>
2024-07-06 04:47:58 +0100

    Task 4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that fetches url using request package"""
+import requests
+
+
+if __name__ == '__main__':
+    f = requests.get('https://alx-intranet.hbtn.io/status')
+    status = f.text
+    print("Body response:")
+    print("\t- type: {}".format(type(status)))
+    print("\t- content: {}".format(status))
