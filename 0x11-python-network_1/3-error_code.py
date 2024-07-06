8cba840fe4d383d94181ed6b49a66358f89ba90d
Fortune Iheanacho <nachodev369@gmail.com>
2024-07-06 04:46:45 +0100

    Task 3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request
+to the URL and displays the body of the response
+(decoded in utf-8).
+"""
+
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as f:
+            print(f.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
