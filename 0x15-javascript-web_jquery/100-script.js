a6dfb42a95afbf7d896d3dbf61e14dd1f49ef5f5
Fortune Iheanacho <nachodev369@gmail.com>
2024-08-08 19:25:16 +0100

    Advanced Tasks.

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+// JavaScript script that updates the text
+// color of the <header> element to red (#FF0000)
+
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header')
+    header.style.color = 'red'
+});
